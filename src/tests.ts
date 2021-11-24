7c78a3c19f3d6a4680a22cccd1a319ff74f90296
Tanishq Kancharla <tklightningfast@gmail.com>
2021-11-24 18:50:44 -0500

    more tests

--- src/tests.ts
+++ src/tests.ts
@@ -1,5 +1,8 @@
 import test0 from "../samples/test.json";
 import alice from "../samples/alice.txt";
+import alice2 from "../samples/alice-2x.txt";
+import alice4 from "../samples/alice-4x.txt";
+import meta from "../samples/meta-mod.txt";
 
 export const TEST_CONTENT = {
   type: "doc",
@@ -9,9 +12,8 @@ export const TEST_CONTENT = {
 let _tests = [];
 
 if (TEST) {
-  _tests.push(test0);
-  _tests.push(
-    (alice as string).split("").map((c, i) => ({
+  const toTransactions = (x: string) =>
+    x.split("").map((c, i) => ({
       steps: [
         {
           stepType: "replace",
@@ -20,8 +22,12 @@ if (TEST) {
           slice: { content: [{ type: "text", text: c }] },
         },
       ],
-    }))
-  );
+    }));
+  _tests.push(test0);
+  _tests.push(toTransactions(alice));
+  _tests.push(toTransactions(alice2));
+  _tests.push(toTransactions(alice4));
+  _tests.push(toTransactions(meta));
 }
 
 export const tests = _tests;

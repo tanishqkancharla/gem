314c82cd6dad633c9458e5d922ae46211be91f32
Tanishq Kancharla <tklightningfast@gmail.com>
2021-12-04 09:32:24 -0500

    fix

--- src/initial.ts
+++ src/initial.ts
@@ -9,75 +9,16 @@ const welcomes = [
   "Go ahead, try me >:)",
   "Just type it out.",
 ];
-const about = {
-  type: "doc",
-  content: [
-    {
-      type: "paragraph",
-      attrs: { type: "base" },
-      content: [
-        {
-          type: "text",
-          text: "Hello! Welcome to Gem. Gem is a light-weight, performant, and minimal editor designed to be aesthetic and fun. ",
-        },
-      ],
-    },
-    { type: "paragraph", attrs: { type: "base" } },
-    {
-      type: "paragraph",
-      attrs: { type: "base" },
-      content: [
-        {
-          type: "text",
-          text: "It's entirely local: your notes don't get sent anywhere, and if you close the tab, they're gone forever.",
-        },
-      ],
-    },
-    { type: "paragraph", attrs: { type: "base" } },
-    {
-      type: "paragraph",
-      attrs: { type: "base" },
-      content: [
-        {
-          type: "text",
-          text: "It currently supports some limited Markdown-like functionality like ",
-        },
-        { type: "text", marks: [{ type: "bold" }], text: "*bold*" },
-        { type: "text", text: ", " },
-        { type: "text", marks: [{ type: "italic" }], text: "_italic_," },
-        { type: "text", text: " and " },
-        { type: "text", marks: [{ type: "code" }], text: "`code`" },
-        {
-          type: "text",
-          text: " although they can be a little buggy. There's plans to add more elements with more robust support.",
-        },
-      ],
-    },
-    { type: "paragraph", attrs: { type: "base" } },
-    {
-      type: "paragraph",
-      attrs: { type: "base" },
-      content: [
-        {
-          type: "text",
-          text: "It's being developed by [Tanishq Kancharla](https://moonrise.tk) (me!) and open-sourced on [Github](https://github.com/moonrise-tk/editor). Have a look at the README for more information.",
-        },
-      ],
-    },
-    { type: "paragraph", attrs: { type: "base" } },
-    {
-      type: "paragraph",
-      attrs: { type: "base" },
-      content: [
-        {
-          type: "text",
-          text: "Please reach out to me if you like Gem, or have any ideas for features!",
-        },
-      ],
-    },
-    { type: "paragraph", attrs: { type: "base" } },
-    { type: "paragraph", attrs: { type: "base" } },
-  ],
-};
+const about = `Hello! Welcome to Gem. Gem is a light-weight, performant, and minimal editor designed to be aesthetic and fun. 
+
+It's entirely local: your notes don't get sent anywhere, and if you close the tab, they're gone forever.
+
+It's being developed by [Tanishq Kancharla](https://moonrise.tk) (me!) and open-sourced on [Github](https://github.com/moonrise-tk/gem). Have a look at the README for more information.
+
+Please reach out to me if you like Gem, or have any ideas for features!
+
+`;
 export const initalContent =
-  welcomes[Math.floor(Math.random() * welcomes.length)];
+  window.location.pathname === "/about"
+    ? about
+    : welcomes[Math.floor(Math.random() * welcomes.length)];

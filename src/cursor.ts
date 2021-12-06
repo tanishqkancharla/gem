5940cf928444ce9c6207cba5c6f8857506a18a26
Tanishq Kancharla <tklightningfast@gmail.com>
2021-12-05 20:19:08 -0500

    implement split cursor

--- src/cursor.ts
+++ src/cursor.ts
@@ -1,27 +1,36 @@
-import { Plugin, PluginKey } from "prosemirror-state";
+import { Plugin, PluginKey, TextSelection } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import { main } from "./index";
 import { schema } from "./schema";
 
 // A cursor class to handle repositioning and deactivation timeouts
 class Cursor {
-  #el: HTMLDivElement;
+  #selectionType: "cursor" | "text";
+  #anchorEl: HTMLDivElement;
+  #headEl: HTMLDivElement;
   #cursorTimeout = 10000;
   #cursorTimeoutFn: number = 0;
 
   constructor() {
-    this.#el = document.createElement("div");
-    this.#el.classList.add("cursor");
-    main.appendChild(this.#el);
+    this.#anchorEl = document.createElement("div");
+    this.#anchorEl.classList.add("cursor", "anchor");
+    this.#headEl = document.createElement("div");
+    this.#headEl.classList.add("cursor", "head");
+    this.#selectionType = "cursor";
+    // this.#headEl.style.display = "none";
+    main.appendChild(this.#anchorEl);
+    main.appendChild(this.#headEl);
   }
 
   activate(): Cursor {
-    this.#el.classList.remove("inactive");
+    this.#headEl.classList.remove("inactive");
+    this.#anchorEl.classList.remove("inactive");
     return this;
   }
 
   deactivate(): Cursor {
-    this.#el.classList.add("inactive");
+    this.#headEl.classList.add("inactive");
+    this.#anchorEl.classList.add("inactive");
     return this;
   }
 
@@ -36,17 +45,69 @@ class Cursor {
   }
 
   // In viewport coordinates
-  reposition(x: number, y: number): Cursor {
-    this.#el.style.transform = `translate(${x - 2}px, ${
+  repositionAnchor(x: number, y: number): Cursor {
+    this.#anchorEl.style.transform = `translate(${x - 2}px, ${
       window.scrollY + y - 4
     }px)`;
     return this;
   }
 
-  repositionToViewAnchor(view: EditorView): Cursor {
+  // In viewport coordinates
+  repositionHead(x: number, y: number): Cursor {
+    this.#headEl.style.transform = `translate(${x - 2}px, ${
+      window.scrollY + y - 4
+    }px)`;
+    return this;
+  }
+
+  repositionToCoords(x: number, y: number): Cursor {
+    return this.repositionAnchor(x, y).repositionHead(x, y);
+  }
+
+  reposition(view: EditorView<typeof schema>): Cursor {
+    const isCursorSelection =
+      view.state.selection.head === view.state.selection.anchor;
     // If you do an all selection, the anchor gets set to 0...for some reason :/
-    const coords = view.coordsAtPos(Math.max(view.state.selection.anchor, 1));
-    this.reposition(coords.right, coords.top);
+    if (isCursorSelection) {
+      const anchorPosition = Math.max(view.state.selection.anchor, 1);
+      const anchorCoords = view.coordsAtPos(anchorPosition);
+      this.repositionToCoords(anchorCoords.right, anchorCoords.top);
+      if (this.#selectionType === "text") {
+        this.#anchorEl.classList.remove("split");
+        this.#headEl.classList.remove("split");
+        this.#selectionType = "cursor";
+      }
+    } else {
+      if (this.#selectionType === "cursor") {
+        this.#anchorEl.classList.add("split");
+        this.#headEl.classList.add("split");
+        this.#selectionType = "text";
+      }
+      // highlight selection
+      const anchorPosition = Math.max(view.state.selection.anchor, 1);
+      const headPosition = Math.min(
+        view.state.selection.head,
+        // This is the last position in the document
+        view.state.doc.nodeSize - 3
+      );
+      if (anchorPosition > headPosition) {
+        this.#anchorEl.classList.add("right");
+        this.#headEl.classList.remove("right");
+        this.#anchorEl.classList.remove("left");
+        this.#headEl.classList.add("left");
+      } else {
+        this.#anchorEl.classList.add("left");
+        this.#headEl.classList.remove("left");
+        this.#anchorEl.classList.remove("right");
+        this.#headEl.classList.add("right");
+      }
+      const anchorCoords = view.coordsAtPos(anchorPosition);
+      this.repositionAnchor(anchorCoords.right, anchorCoords.top);
+
+      const headCoords = view.coordsAtPos(headPosition);
+      this.repositionHead(headCoords.right, headCoords.top);
+    }
+
     return this;
   }
 }
@@ -56,16 +117,17 @@ export const CursorPlugin = new Plugin<void, typeof schema>({
   view: (view) => {
     const cursor = new Cursor();
     window.addEventListener("resize", () => {
-      cursor.repositionToViewAnchor(view);
+      cursor.reposition(view);
     });
-    view.root.addEventListener("focus", () => cursor.resetTimeout(), true);
 
+    view.root.addEventListener("focus", () => cursor.resetTimeout(), true);
     view.root.addEventListener("blur", () => cursor.deactivate(), true);
-    cursor.repositionToViewAnchor(view);
+
+    cursor.reposition(view);
     return {
       update: (view) => {
         cursor.resetTimeout();
-        cursor.repositionToViewAnchor(view);
+        cursor.reposition(view);
       },
     };
   },

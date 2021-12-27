f8893ad0649fbafecba6f20f08406ada8356b935
Tanishq Kancharla <tklightningfast@gmail.com>
2021-12-26 23:51:57 -0500

    more themes

--- build.js
+++ build.js
@@ -4,6 +4,9 @@ const { build } = require("estrella");
 build({
   entry: ["./src/index.ts", "./src/style.css"],
   outdir: "dist",
+  loader: {
+    ".ttf": "file",
+  },
   bundle: true,
   minify: true,
 });

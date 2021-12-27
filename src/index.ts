ea7f346ea7d82f7d76bd1aad429b308bcd784543
Tanishq Kancharla <tklightningfast@gmail.com>
2021-12-26 19:32:32 -0500

    basic syntax highlighting

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import { schema } from "./schema";
+import { baseKeymap } from "prosemirror-commands";
+import { history, redo, undo } from "prosemirror-history";
+import { keymap } from "prosemirror-keymap";
+import { Node } from "prosemirror-model";
 import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
-import { Node } from "prosemirror-model";
-import { undo, redo, history } from "prosemirror-history";
-import { keymap } from "prosemirror-keymap";
-import { baseKeymap } from "prosemirror-commands";
-import { markdownInputRules, markdownKeyBindings } from "./markdown";
 import { CursorPlugin } from "./cursor";
 import { initalContent } from "./initial";
+import { markdownKeyBindings } from "./markdown";
+import { schema } from "./schema";
+import { SyntaxHighlightPlugin } from "./syntax";
 
 export const main = document.querySelector("main")!;
 
@@ -23,7 +24,8 @@ const state = EditorState.create<typeof schema>({
       ...markdownKeyBindings,
     }),
     keymap<typeof schema>(baseKeymap),
-    markdownInputRules,
+    SyntaxHighlightPlugin,
+    // markdownInputRules,
     CursorPlugin,
   ],
 });
